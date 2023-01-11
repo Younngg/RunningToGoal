905ba29ce4712e94106dfe04abcb36d60a5bc10b
younngg <8533283@naver.com>
2023-01-11 12:25:45 +0900

    design: #2-버튼 hover 시 안내 멘트 구현

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -16,9 +16,12 @@ const ProgressBar: FC<ProgressBarProps> = ({ data, onClickEditCurrent }) => {
     <Container>
       <Bar percent={getPercent()}>
         <div>
-          <EditCurrentButton onClick={() => onClickEditCurrent(data)}>
-            <RunningMan src={`${process.env.PUBLIC_URL}/run2.png`} />
-          </EditCurrentButton>
+          <EditCurrent>
+            <EditCurrentButton onClick={() => onClickEditCurrent(data)}>
+              <RunningMan src={`${process.env.PUBLIC_URL}/run2.png`} />
+            </EditCurrentButton>
+            <p className='infoMessage'>현재 값을 수정하려면 클릭해주세요!</p>
+          </EditCurrent>
           <CurrentText percent={getPercent()}>
             {data.current}
             {data.unit}
@@ -46,7 +49,7 @@ const Bar = styled.div<{ percent: number }>`
   border-radius: 25px;
   position: relative;
 
-  div {
+  & > div {
     width: ${({ percent }) => percent}%;
     height: 2rem;
     background-color: ${({ theme }) => theme.color.pink};
@@ -78,14 +81,37 @@ const GoalText = styled.span`
   text-align: center;
 `;
 
+const EditCurrent = styled.div`
+  position: absolute;
+  right: -2rem;
+  top: -4rem;
+  width: fit-content;
+
+  .infoMessage {
+    display: none;
+    position: absolute;
+    font-size: 1.3rem;
+    line-height: 1.6;
+    width: 14rem;
+    padding: 0.5rem;
+    background-color: white;
+    border-radius: 5px;
+    z-index: 100;
+    box-shadow: 0px 0px 7px 0px rgba(0, 0, 0, 0.2);
+  }
+
+  &:hover {
+    .infoMessage {
+      display: block;
+    }
+  }
+`;
+
 const RunningMan = styled.img`
   width: 4rem;
 `;
 
 const EditCurrentButton = styled.button`
-  position: absolute;
-  right: -2rem;
-  top: -4rem;
   width: fit-content;
   padding: 0;
   background-color: transparent;
