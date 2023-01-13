0d4f1007c85acc3cbdf5597532a1a9faf6267d03
younngg <8533283@naver.com>
2023-01-13 13:57:22 +0900

    feat: 로그인 기능 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../App';
+import Button from '../components/Button/Button';
 
 const Login = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const goToHome = (userId: string) => {
+    navigate('/', { state: { id: userId } });
+  };
+
+  const onLogin = async () => {
+    try {
+      const data = await authService.login('Google');
+      goToHome(data.user.uid);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user: { id: string }) => {
+      user && goToHome(user.id);
+    });
+  });
+
+  return (
+    <div>
+      <Button text='로그인' onClick={onLogin} />
+    </div>
+  );
 };
 
 export default Login;
